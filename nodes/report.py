af1d400d6b7e348f0eb2b251bcbca32599022f69
sanyam280 <sanyam.code280@gmail.com>
2025-12-16 01:38:00 +0530

    added report_writer node

---
+++ nodes/report.py
@@ -0,0 +1,34 @@
+from fpdf import FPDF
+from core.state import AgentState
+
+def pdf_node(state:AgentState) -> None:
+    """
+    Takes the final report and save it as a PDF.
+    """
+
+    print("Generating PDF....")
+    
+    report_text = state['final_report']
+    filename = f"{state['topic'].replace(' ', '_')}_Report.pdf"
+
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+
+    #Add Title
+    pdf.set_font("Arial", 'B', 16)
+    pdf.cell(200, 10, txt= f"Research Report: {state['topic']}", ln=1, align='C')
+
+
+    #Body
+    pdf.set_font("Arial", size=12)
+    # multi_cell handles text wrapping automatically
+    # encode/decode helps strip non-latin characters that crash basic FPDF
+    safe_text = report_text.encode('latin-1', 'replace').decode('latin-1')
+    pdf.multi_cell(0, 10, txt=safe_text)
+    
+    pdf.output(filename)
+    print(f"Success! PDF Saved: {filename}")
+    return
+
+
